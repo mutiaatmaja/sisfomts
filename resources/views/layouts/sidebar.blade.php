3e9d2dd31712a35a56c2e4267a49d20739eaae5c
mutiaatmaja <mutiaatmaja80@guru.smk.belajar.id>
2025-05-10 13:13:37 +0700

    update tampilan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,12 +5,12 @@
         <div class="navbar-nav theme-brand flex-row  text-center">
             <div class="nav-logo">
                 <div class="nav-item theme-logo">
-                    <a href="index-2.html">
-                        {{-- <img src="https://designreset.com/cork/html/src/assets/img/logo.svg" class="navbar-logo" alt="logo"> --}}
+                    <a href="index.html">
+                        <img src="src/assets/img/g-8.png" class="navbar-logo" alt="logo">
                     </a>
                 </div>
                 <div class="nav-item theme-text">
-                    <a href="index-2.html" class="nav-link"> MTS </a>
+                    <a href="index.html" class="nav-link"> MTS </a>
                 </div>
             </div>
             <div class="nav-item sidebar-toggle">
@@ -19,17 +19,22 @@
                 </div>
             </div>
         </div>
+
+        <div class="profile-info">
+            <div class="user-info">
+                <div class="profile-img">
+                    <img src="layouts/src/assets/img/profile-30.png" alt="avatar">
+                </div>
+                <div class="profile-content">
+                    <h6 class="">{{ auth()->user()->name ?? 'Tamu' }}</h6>
+                    <p class="">Sistem Informasi</p>
+                </div>
+            </div>
+        </div>
+
         <div class="shadow-bottom"></div>
         <ul class="list-unstyled menu-categories" id="accordionExample">
-            <li class="menu @if(request()->is('/')) active @endif">
-                <a href="app-calendar.html" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                        <span>Beranda</span>
-                    </div>
-                </a>
-            </li>
-            {{-- <li class="menu">
+            <li class="menu active">
                 <a href="#dashboard" data-bs-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -41,23 +46,31 @@
                 </a>
                 <ul class="collapse submenu list-unstyled show" id="dashboard" data-bs-parent="#accordionExample">
                     <li class="active">
-                        <a href="index-2.html"> Analytics </a>
+                        <a href="index.html"> Analytics </a>
                     </li>
                     <li>
                         <a href="index2.html"> Sales </a>
                     </li>
                 </ul>
-            </li> --}}
+            </li>
+            <li class="menu {{ request()->is('/') ? 'active' : '' }}">
+                <a href="/" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                        <span>Beranda</span>
+                    </div>
+                </a>
+            </li>
 
             <li class="menu menu-heading">
-                <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg><span>APLIKASI</span></div>
+                <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg><span>APPLICATIONS</span></div>
             </li>
 
             <li class="menu">
                 <a href="app-calendar.html" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                        <span>Absensi</span>
+                        <span>Calendar</span>
                     </div>
                 </a>
             </li>
@@ -66,46 +79,10 @@
                 <a href="app-chat.html" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                        <span>Peserta Didik</span>
+                        <span>Chat</span>
                     </div>
                 </a>
             </li>
-
-            <li class="menu">
-                <a href="app-chat.html" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                        <span>Alumni</span>
-                    </div>
-                </a>
-            </li>
-            <li class="menu">
-                <a href="app-chat.html" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                        <span>Guru dan TU</span>
-                    </div>
-                </a>
-            </li>
-            <li class="menu">
-                <a href="app-chat.html" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                        <span>SarPras</span>
-                    </div>
-                </a>
-            </li>
-            <li class="menu">
-                <a href="app-chat.html" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                        <span>Prestasi</span>
-                    </div>
-                </a>
-            </li>
-
-
-
 
             <li class="menu">
                 <a href="app-mailbox.html" aria-expanded="false" class="dropdown-toggle">
@@ -412,10 +389,10 @@
                         <a href="layout-empty.html"> Empty Page </a>
                     </li>
                     <li>
-                        <a href="layout-boxed.html"> Boxed </a>
+                        <a href="layout-full-width.html"> Full Width </a>
                     </li>
                     <li>
-                        <a href="layout-collapsible-menu.html"> Collapsed menu  </a>
+                        <a href="layout-collapsible-menu.html"> Collapsed Menu </a>
                     </li>
                 </ul>
             </li>
