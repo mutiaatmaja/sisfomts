d9200518badcde096699fd317322274850e52216
mutiaatmaja <mutiaatmaja0@guru.smk.belajar.id>
2025-06-17 17:46:40 +0700

    foto osis

--- resources/views/osis/index.blade.php
+++ resources/views/osis/index.blade.php
@@ -19,7 +19,7 @@
                         <div class="card">
                             <div class="card-body text-center">
                                 <img src="{{ asset('storage/' . $osis->siswa->user->foto) }}" alt="{{ $osis->siswa->user->name }}"
-                                     class="rounded-circle mb-3" style="width: 100px; height: 100px; object-fit: cover;">
+                                     class=" mb-3" style=" object-fit: cover;">
                                 <h5 class="card-title">{{ $osis->siswa->user->name }}</h5>
                                 <p class="card-text text-primary">{{ $osis->jabatan }}</p>
                                 <p class="card-text text-muted">{{ $osis->periode }}</p>
