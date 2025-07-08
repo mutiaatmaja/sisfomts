c49550cba0fb1ee585b4efe53e2c675fcf5d0b69
mutiaatmaja <mutiaatmaja80@guru.smk.belajar.id>
2025-07-08 19:02:53 +0700

    menambah link ke perpus

--- resources/views/akademik/index.blade.php
+++ resources/views/akademik/index.blade.php
@@ -4,7 +4,7 @@
 
 
         <div class="row layout-top-spacing">
-            <div class="col-xl-3 col-lg-6 col-md-6  mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-3  mb-4">
                 <div class="card">
                     <img src="{{ asset('gambarutama/Elearning.png') }}"  class="card-img-top" alt="...">
                     <div class="card-body">
@@ -14,7 +14,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-6 col-md-6  mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-3  mb-4">
                 <div class="card">
                     <img src="{{ asset('gambarutama/cbt.png') }}"  class="card-img-top" alt="...">
                     <div class="card-body">
@@ -25,7 +25,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-6 col-md-6  mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-3  mb-4">
                 <div class="card">
                     <img src="{{ asset('gambarutama/rdm.png') }}"  class="card-img-top" alt="...">
                     <div class="card-body">
@@ -35,13 +35,23 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-6 col-md-6  mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-3  mb-4">
                 <div class="card">
                     <img src="{{ asset('gambarutama/akmi.png') }}"  class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title mb-3">AKMI</h5>
                         <p class="card-text">Asesmen Kompetensi Madrasah Indonesia<br /><br /></p>
                         <a href="https://portal-akmi.kemenag.go.id/" target="_blank" class="btn btn-secondary mt-3 w-100">Ke Aplikasi</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-3  mb-4">
+                <div class="card">
+                    <img src="https://perpustakaan.mtsn1mempawah.sch.id/uploaded_files/aplikasi/inlislite.png"  class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title mb-3">Perpustakaan</h5>
+                        <p class="card-text">Perpustakaan MTs N1 "NURUL ILMI" Mempawah<br /><br /></p>
+                        <a href="https://perpustakaan.mtsn1mempawah.sch.id" target="_blank" class="btn btn-secondary mt-3 w-100">Ke Aplikasi</a>
                     </div>
                 </div>
             </div>
